fdc043b01dc51a16c9b92adc60a7469aee463dbe
naveen pokhrel <naveenpokhrel99@gmail.com>
2026-01-27 23:41:14 +0545

    all set in nav and pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to our home page. Explore our store and learn more about us.</p>
+    </div>
+  )
+}
---
+++ src/pages/Store.tsx
@@ -0,0 +1,8 @@
+export function Store() {
+  return (
+    <div>
+      <h1>Store Page</h1>
+      <p>Welcome to the store! Browse our products here.</p>
+    </div>
+  )
+}
