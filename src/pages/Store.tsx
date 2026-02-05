9f1154e86eb34388d8fbc0d016b3cb959153d866
naveen pokhrel <naveenpokhrel99@gmail.com>
2026-02-05 23:23:59 +0545

    updated all the code

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,20 +1,19 @@
-import Col from 'react-bootstrap/esm/Col'
-import storeItems from '../data/items.json'
-import Row from 'react-bootstrap/esm/Row'
-import { StoreItems } from '../components/StoreItem'
-
-
+import Col from "react-bootstrap/esm/Col";
+import storeItems from "../data/items.json";
+import Row from "react-bootstrap/esm/Row";
+import { StoreItem } from "../components/StoreItem";
 
 export function Store() {
   return (
     <>
       <h1>Store Page</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
-        {storeItems.map(item => (
-          <Col key={item.id}><StoreItems {...item} />
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
           </Col>
         ))}
       </Row>
     </>
-  )
+  );
 }
